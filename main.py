b61b94349f742a91a2effa5a3d650fc762d4e6bb
0ritam <mujritam085@gmail.com>
2025-05-24 18:14:59 +0530

    Video transcribtion done

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+import json
 import pathlib
+import subprocess
+import time
 import uuid
 import boto3
 from fastapi import Depends, HTTPException, status
@@ -6,6 +9,8 @@ from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
 import modal
 from pydantic import BaseModel
 import os
+
+import whisperx
 
 class ProcessVideoRequest(BaseModel):
     s3_key: str
@@ -31,7 +36,46 @@ class AiPodcastClipper:
     @modal.enter()
     def load_mode(self):
         print("loading models")
-        pass
+
+        self.whisperx_model = whisperx.load_model("large-v2", device="cuda", compute_type="float16")
+
+        self.alignment_model, self.metadata= whisperx.load_align_model(
+            language_code = "en",
+            device = "cuda"
+
+        )
+
+        print("Transciption models loaded")
+
+
+    
+        
+
+    def transcribe_video(self, base_dir: str, video_path: str) -> str:
+        audio_path = base_dir / "audio.wav"
+        extract_cmd = f"ffmpeg -i {video_path} -vn -acodec pcm_s16le -ar 16000 -ac 1 {audio_path}"
+        subprocess.run(extract_cmd, shell=True,
+                       check=True, capture_output=True)
+
+        print("Starting transcription with WhisperX...")
+        start_time = time.time()
+
+        audio = whisperx.load_audio(str(audio_path))
+        result = self.whisperx_model.transcribe(audio, batch_size=16)
+
+        result = whisperx.align(
+            result["segments"],
+            self.alignment_model,
+            self.metadata,
+            audio,
+            device="cuda",
+            return_char_alignments=False
+        )
+
+        duration = time.time() - start_time
+        print("Transcription and alignment took " + str(duration) + " seconds")
+
+        print(json.dumps(result, indent=2))
 
     @modal.fastapi_endpoint(method="POST")
     def process_video(self,request:ProcessVideoRequest, token: HTTPAuthorizationCredentials = Depends(auth_scheme)):
@@ -54,6 +98,8 @@ class AiPodcastClipper:
         )
 
         print(f"Files in {base_dir}:", os.listdir(base_dir))
+
+        self.transcribe_video(base_dir, video_path)
 
 @app.local_entrypoint()
 def main():
